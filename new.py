45011d339923bb88e0873b6d7fee9fe62d1b3d2b
chamudiabeysinghe <chamudi.20200489@iit.ac.lk>
2021-06-25 19:25:36 +0530

    third change

--- new.py
+++ new.py
@@ -4,3 +4,10 @@ for i in a:
     if i > max_num:
         max_num = i
 print(max_num)
+
+xy = [50, 2, 34, 6, 4, 3, 1, 5, 2] 
+t=0
+for i in range(len(xy)):
+    if xy[i]<xy[t]:
+        t=i
+print t
